23dd9c77542882fee555d52a44805e9759e2c58b
Alex Resnick <adr8292@gmail.com>
2026-01-18 11:48:45 -0600

    It works

--- vendor-update/vendor-update.py
+++ vendor-update/vendor-update.py
@@ -63,13 +63,14 @@ def get_project() -> str | None:
         "X-GitHub-Api-Version": "2022-11-28",
         "Accept": "application/vnd.github+json",
     }
-    projects: list[dict[str, str]] = requests.get(url, headers=headers)
-    projects.raise_for_status()
+    resp = requests.get(url, headers=headers)
+    resp.raise_for_status()
     proj_year = int(getProjectYear())
     current_year = date.today().year
     year = max(proj_year, current_year)
-    projects = [pro for pro in projects.json() if str(year) in pro.get("title")]
-    projects.sort(key="number", reverse=True)
+    projects: list[dict[str, str | int]] = resp.json()
+    projects = [pro for pro in projects if str(year) in pro.get("title")]
+    # projects.sort(key="number", reverse=True)
     if len(projects) == 0:
         return None
     return projects[0].get("number", None)
